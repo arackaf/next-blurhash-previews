637a9e638ed865804dca8ddacb5f302ae4426cb6
Adam Rackis <adamrackis@hotmail.com>
2022-07-02 23:45:45 -0400

    html parsing and formatting

--- plugin.js
+++ plugin.js
@@ -1,6 +1,9 @@
 import { visit } from "unist-util-visit";
 import { visitParents } from "unist-util-visit-parents";
 
+import HTMLParser from "node-html-parser";
+import prettier from "prettier";
+
 export default function retextSentenceSpacing() {
   return (tree, file) => {
     visitParents(tree, "text", (node, ancestors) => {
@@ -12,13 +15,15 @@ export default function retextSentenceSpacing() {
 
         const parent = ancestors[ancestors.length - 1];
         const index = parent.children.indexOf(node);
+
+        const newNode = HTMLParser.parse(`<div>
+        <img />
+        <span>Hey there</span>
+        </div>`);
+
         parent.children[index] = {
           type: "html",
-          value: `<div>
-  <img />
-  <span>Hey there</span>
-</div>`,
-          position: node.position,
+          value: prettier.format(newNode.outerHTML, { parser: "html" }).trimEnd(),
         };
         //parent.children[index] = replacement;
 
