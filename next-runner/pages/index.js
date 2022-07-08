a258a9ad506691af015129ed088ebe054b0e7e65
Adam Rackis <adamrackis@hotmail.com>
2022-07-08 09:59:05 -0500

    Small test runner style tweaks

--- next-runner/pages/index.js
+++ next-runner/pages/index.js
@@ -13,14 +13,16 @@ export default function Home() {
       <br />
       <button onClick={() => setVal((x) => x + 1)}>Refresh</button>
       {val}
-      <uikit-image key={val} url="/dynamo-introduction/img1.png" preview='{ "w": 364, "h": 196, "blurhash": "L8S6SsD*%gt7IVM|tRRj~qWBM{NG" }'>
-        <img slot="image" src="/dynamo-introduction/img1.png" />
-        <canvas slot="preview" width="364" height="196"></canvas>
-      </uikit-image>
-      <uikit-image key={val + 9} url="/dynamo-introduction/img5a.png" preview='{ "w": 2244, "h": 622, "blurhash": "LnPGmj_zIA:KS}j[s:ay-VWVRjay" }'>
-        <img slot="image" src="/dynamo-introduction/img5a.png" />
-        <canvas slot="preview" width="2244" height="622"></canvas>
-      </uikit-image>
+      <div style={{ display: "flex" }}>
+        <uikit-image key={val} url="/dynamo-introduction/img1.png" preview='{ "w": 364, "h": 196, "blurhash": "L8S6SsD*%gt7IVM|tRRj~qWBM{NG" }'>
+          <img slot="image" src="/dynamo-introduction/img1.png" />
+          <canvas slot="preview" width="364" height="196"></canvas>
+        </uikit-image>
+        <uikit-image key={val + 9} url="/dynamo-introduction/img5a.png" preview='{ "w": 500, "h": 139, "blurhash": "LnPGmj_zIA:KS}j[s:ay-VWVRjay" }'>
+          <img slot="image" src="/dynamo-introduction/img5a.png" />
+          <canvas slot="preview" width="500" height="139"></canvas>
+        </uikit-image>
+      </div>
     </div>
   );
 }
