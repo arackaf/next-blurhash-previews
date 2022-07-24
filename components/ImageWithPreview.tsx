affd1bc9476017a942b2cffc927cfa84651c85ea
Adam Rackis <adamrackis@hotmail.com>
2022-07-23 19:16:27 -0500

    More web component cleanup

--- components/ImageWithPreview.tsx
+++ components/ImageWithPreview.tsx
@@ -6,12 +6,12 @@ class ImageWithPreview extends HTMLElement {
   sd: ShadowRoot;
   mo?: MutationObserver;
 
-  static observedAttributes = ["preview", "url"];
+  static observedAttributes = ["preview"];
 
-  get imgEl(): any {
+  get #imgEl(): any {
     return this.querySelector("img");
   }
-  get canvasEl(): any {
+  get #canvasEl(): any {
     return this.querySelector("canvas");
   }
 
@@ -22,17 +22,24 @@ class ImageWithPreview extends HTMLElement {
     this.sd.innerHTML = `<slot name="preview"></slot>`;
   }
 
-  checkReady = () => {
-    if (this.imgEl && this.canvasEl) {
+  #checkReady = () => {
+    if (this.#imgEl && this.#canvasEl) {
       this.mo?.disconnect();
-      this.ready();
+
+      if (this.#imgEl.complete) {
+        this.#imgLoad();
+      } else {
+        this.#updatePreview();
+        this.#imgEl.addEventListener("load", this.#imgLoad);
+      }
+
       return true;
     }
   };
 
   connectedCallback() {
-    if (!this.checkReady()) {
-      this.mo = new MutationObserver(this.checkReady);
+    if (!this.#checkReady()) {
+      this.mo = new MutationObserver(this.#checkReady);
       this.mo.observe(this, {
         subtree: true,
         childList: true,
@@ -41,28 +48,19 @@ class ImageWithPreview extends HTMLElement {
     }
   }
 
-  ready() {
-    if (this.imgEl.complete) {
-      this.imgLoad();
-    } else {
-      this.updatePreview();
-      this.imgEl.addEventListener("load", this.imgLoad);
-    }
-  }
-
-  imgLoad = () => {
+  #imgLoad = () => {
     this.sd.innerHTML = `<slot name="image"></slot>`;
   };
 
   attributeChangedCallback(name) {
-    if (this.canvasEl && name === "preview") {
-      this.updatePreview();
+    if (this.#canvasEl && name === "preview") {
+      this.#updatePreview();
     }
   }
 
-  updatePreview() {
+  #updatePreview() {
     const previewObj = JSON.parse(this.getAttribute("preview")!);
-    updateBlurHashPreview(this.canvasEl, previewObj);
+    updateBlurHashPreview(this.#canvasEl, previewObj);
   }
 }
 
